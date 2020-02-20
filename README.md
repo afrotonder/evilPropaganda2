cffe54ef346276f0ae4840b1af94b136da4e45ed
Omar Rosado Ramírez <o.rosado18@gmail.com>
2020-02-20 16:16:19 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# evilPropaganda2
-Created with CodeSandbox
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<div class="imgwrap">
+    <div class="img">
+      <h1> EMISSARY &nbsp&nbsp OF &nbsp&nbsp DEATH </h1>  
+    </div>
+
+</div>
+`;
